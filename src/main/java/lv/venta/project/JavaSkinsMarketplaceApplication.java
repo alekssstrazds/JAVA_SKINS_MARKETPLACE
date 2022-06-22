4201eef71097635800b9561ddcfae7cc80951132
s20straalek <s20straalek@vea.lv>
2022-06-22 12:51:03 +0300

    PROJECT CREATEDgit add .!

---
+++ src/main/java/lv/venta/project/JavaSkinsMarketplaceApplication.java
@@ -0,0 +1,13 @@
+package lv.venta.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSkinsMarketplaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSkinsMarketplaceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lv/venta/project/JavaSkinsMarketplaceApplicationTests.java
@@ -0,0 +1,13 @@
+package lv.venta.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSkinsMarketplaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
